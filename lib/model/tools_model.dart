97a8f80cbf21f9436cf552b892cc25f76cab8363
karthikkodiyil <karthikkodiyil@gmail.com>
2025-01-13 23:28:44 +0530

    demo

--- lib/model/tools_model.dart
+++ lib/model/tools_model.dart
@@ -14,8 +14,85 @@ List<ToolsModel> toolList = [
   ToolsModel(
     img: Assets.image.toolkitTools.path,
     name: "Abcd",
+    prize: "250",
+    oldPrize: "399",
+    disc: "This was a all terrain agriculture tool that used to pluck grass and unwanted stones on the cultivating land that was highly realable and as well as good for your land ."
+  ),
+   ToolsModel(
+    img: Assets.image.toolkitTools1.path,
+    name: "ghij",
     prize: "389",
     oldPrize: "599",
+    disc: "This was a all terrain agriculture tool that used to pluck grass and unwanted stones on the cultivating land that was highly realable and as well as good for your land ."
+  ),
+   ToolsModel(
+    img: Assets.image.toolkitTools2.path,
+    name: "klmn",
+    prize: "199",
+    oldPrize: "399",
+    disc: "This was a all terrain agriculture tool that used to pluck grass and unwanted stones on the cultivating land that was highly realable and as well as good for your land ."
+  ),
+   ToolsModel(
+    img: Assets.image.toolkitTools3.path,
+    name: "Opqr",
+    prize: "289",
+    oldPrize: "599",
+    disc: "This was a all terrain agriculture tool that used to pluck grass and unwanted stones on the cultivating land that was highly realable and as well as good for your land ."
+  ),
+   ToolsModel(
+    img: Assets.image.toolkitTools4.path,
+    name: "Stuv",
+    prize: "350",
+    oldPrize: "899",
+    disc: "This was a all terrain agriculture tool that used to pluck grass and unwanted stones on the cultivating land that was highly realable and as well as good for your land ."
+  ),
+   ToolsModel(
+    img: Assets.image.toolkitTools5.path,
+    name: "Wxyz",
+    prize: "389",
+    oldPrize: "599",
+    disc: "This was a all terrain agriculture tool that used to pluck grass and unwanted stones on the cultivating land that was highly realable and as well as good for your land ."
+  ),
+   ToolsModel(
+    img: Assets.image.toolkitTools6.path,
+    name: "Bcdf",
+    prize: "450",
+    oldPrize: "650",
+    disc: "This was a all terrain agriculture tool that used to pluck grass and unwanted stones on the cultivating land that was highly realable and as well as good for your land ."
+  ),
+   ToolsModel(
+    img: Assets.image.toolkitTools7.path,
+    name: "Ijkl",
+    prize: "200",
+    oldPrize: "250",
+    disc: "This was a all terrain agriculture tool that used to pluck grass and unwanted stones on the cultivating land that was highly realable and as well as good for your land ."
+  ),
+   ToolsModel(
+    img: Assets.image.toolkitTools8.path,
+    name: "Mnop",
+    prize: "299",
+    oldPrize: "350",
+    disc: "This was a all terrain agriculture tool that used to pluck grass and unwanted stones on the cultivating land that was highly realable and as well as good for your land ."
+  ),
+   ToolsModel(
+    img: Assets.image.toolkitTools9.path,
+    name: "Sfgh",
+    prize: "699",
+    oldPrize: "1099",
+    disc: "This was a all terrain agriculture tool that used to pluck grass and unwanted stones on the cultivating land that was highly realable and as well as good for your land ."
+  ),
+   ToolsModel(
+    img: Assets.image.toolkitTools11.path,
+    name: "Csgh",
+    prize: "459",
+    oldPrize: "999",
+    disc: "This was a all terrain agriculture tool that used to pluck grass and unwanted stones on the cultivating land that was highly realable and as well as good for your land ."
+  ),
+   ToolsModel(
+    img: Assets.image.toolkitTools10.path,
+    name: "Ssjk",
+    prize: "299",
+    oldPrize: "499",
     disc: "This was a all terrain agriculture tool that used to pluck grass and unwanted stones on the cultivating land that was highly realable and as well as good for your land ."
   ),
 ];
--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:toolkit/model/tools_model.dart';
 import 'package:toolkit/utils/app_colors.dart';
 import 'package:toolkit/utils/app_styles.dart';
 import 'package:toolkit/utils/assets/assets.gen.dart';
@@ -16,83 +17,109 @@ class HomeView extends StatelessWidget {
       appBar: AppBar(
         leading: IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
         backgroundColor: kWhite,
-        title: Expanded(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Toolkit",
-                    style: styles.primBold20,
-                  ),
-                  Text(
-                    " Go to greeen",
-                    style: styles.blackRegular11,
-                  ),
-                ],
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 15, right: 15),
-                  child: SizedBox(
-                    height: 40,
-                    child: SearchAnchor.bar(
-                        suggestionsBuilder: (context, searchController) {
-                      return [];
-                    }),
-                  ),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Toolkit",
+                  style: styles.primBold20,
                 ),
-              ),
-              CircleAvatar(
-                backgroundImage: AssetImage(Assets.image.manProfile.path),
-              )
-            ],
-          ),
-        ),
-      ),
-      body: SafeArea(
-          child: Column(
-        children: [
-          15.kH,
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              decoration: const BoxDecoration(),
-              child: CarouselSlider(
-                items: [
-                  _CarouselmageWidget(img: Assets.image.toolkitCarousel.path),
-                  _CarouselmageWidget(img: Assets.image.toolkitCarousel1.path),
-                  _CarouselmageWidget(img: Assets.image.toolkitCarousel2.path),
-                  _CarouselmageWidget(img: Assets.image.toolkitCarousel3.path),
-                  _CarouselmageWidget(img: Assets.image.toolkitCarousel4.path),
-                  _CarouselmageWidget(img: Assets.image.toolkitCarousel5.path),
-                  _CarouselmageWidget(img: Assets.image.toolkitCarousel6.path),
-                  _CarouselmageWidget(img: Assets.image.toolkitCarousel7.path),
-                  _CarouselmageWidget(img: Assets.image.toolkitCarousel8.path),
-                  _CarouselmageWidget(img: Assets.image.toolkitCarousel9.path),
-                  _CarouselmageWidget(img: Assets.image.toolkitCarousel10.path),
-                ],
-                options: CarouselOptions(
-                  height: SizeConfig.screenWidth * 0.2,
-                  //   aspectRatio: 20 / 18,
-                  viewportFraction: 0.9,
-                  initialPage: 0,
-                  enableInfiniteScroll: false,
-                  reverse: false,
-                  autoPlay: true,
-                  autoPlayInterval: const Duration(seconds: 3),
-                  autoPlayAnimationDuration: const Duration(milliseconds: 1000),
-                  autoPlayCurve: Curves.fastOutSlowIn,
-                  enlargeCenterPage: true,
-                  enlargeFactor: 0.8,
-                  scrollDirection: Axis.horizontal,
+                Text(
+                  " Go to greeen",
+                  style: styles.blackRegular11,
+                ),
+              ],
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 15, right: 15),
+                child: SizedBox(
+                  height: 40,
+                  child: SearchAnchor.bar(
+                      suggestionsBuilder: (context, searchController) {
+                    return [];
+                  }),
                 ),
               ),
             ),
-          )
-        ],
+            CircleAvatar(
+              backgroundImage: AssetImage(Assets.image.manProfile.path),
+            )
+          ],
+        ),
+      ),
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              15.kH,
+              Container(
+                decoration: const BoxDecoration(),
+                child: CarouselSlider(
+                  items: [
+                    _CarouselmageWidget(img: Assets.image.toolkitCarousel.path),
+                    _CarouselmageWidget(
+                        img: Assets.image.toolkitCarousel1.path),
+                    _CarouselmageWidget(
+                        img: Assets.image.toolkitCarousel2.path),
+                    _CarouselmageWidget(
+                        img: Assets.image.toolkitCarousel3.path),
+                    _CarouselmageWidget(
+                        img: Assets.image.toolkitCarousel4.path),
+                    _CarouselmageWidget(
+                        img: Assets.image.toolkitCarousel5.path),
+                    _CarouselmageWidget(
+                        img: Assets.image.toolkitCarousel6.path),
+                    _CarouselmageWidget(
+                        img: Assets.image.toolkitCarousel7.path),
+                    _CarouselmageWidget(
+                        img: Assets.image.toolkitCarousel8.path),
+                    _CarouselmageWidget(
+                        img: Assets.image.toolkitCarousel9.path),
+                    _CarouselmageWidget(
+                        img: Assets.image.toolkitCarousel10.path),
+                  ],
+                  options: CarouselOptions(
+                    height: SizeConfig.screenWidth * 0.2,
+                    //   aspectRatio: 20 / 18,
+                    viewportFraction: 0.9,
+                    initialPage: 0,
+                    enableInfiniteScroll: false,
+                    reverse: false,
+                    autoPlay: true,
+                    autoPlayInterval: const Duration(seconds: 3),
+                    autoPlayAnimationDuration:
+                        const Duration(milliseconds: 1000),
+                    autoPlayCurve: Curves.fastOutSlowIn,
+                    enlargeCenterPage: true,
+                    enlargeFactor: 0.8,
+                    scrollDirection: Axis.horizontal,
+                  ),
+                ),
+              ),
+              GridView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: toolList.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2),
+                  itemBuilder: (context, index) {
+                    return Container(
+                      height: 50,
+                      width: 50,
+                      decoration:
+                          BoxDecoration(border: Border.all(color: kBlack)),
+                      // child: Column(),
+                    );
+                  })
+            ],
+          ),
+        ),
       )),
     );
   }
@@ -100,7 +127,7 @@ class HomeView extends StatelessWidget {
 
 class _CarouselmageWidget extends StatelessWidget {
   final String img;
-  const _CarouselmageWidget({super.key, required this.img});
+  const _CarouselmageWidget({required this.img});
 
   @override
   Widget build(BuildContext context) {
